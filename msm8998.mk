c44b663f36fbc14e9647059e52aec1b8afe04023
sbwml <984419930@qq.com>
2021-01-24 19:36:14 +0800

    Revert "msm8998-common: Move spectrum settings"
    This reverts commit 75cb4e77a2ed85eceab7e8b9cb28747435fe6fe6.

--- msm8998.mk
+++ msm8998.mk
@@ -343,6 +343,7 @@ PRODUCT_PACKAGES += \
     init.parts.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
+    init.spectrum.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
